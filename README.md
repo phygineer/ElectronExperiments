5769853e1d6f48d325a3cb85c528f17c1db7b9ad
Phygineer <phygineer@gmail.com>
2024-12-18 07:56:23 +0530

    ipc example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ElectronExperiments
 Experiments with electron js for learning and research
+
+
+### [Inter-Process Communication](https://www.electronjs.org/docs/latest/tutorial/ipc)
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
 const path = require('node:path')
 
-function createWindow () {
+const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -14,6 +14,7 @@ function createWindow () {
 }
 
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
   createWindow()
 
   app.on('activate', () => {
